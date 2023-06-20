557bd08fb14153c373f93f0f7c2d8d0374a7a0ac
Piyush Shukla <piyushshukla356@gmail.com>
2023-06-20 23:16:42 +0530

    feature: everthing move to generic api view

--- watchlist/routes/review.py
+++ watchlist/routes/review.py
@@ -3,28 +3,39 @@ from watchlist.models import Review
 from watchlist.serializers import ReviewSerializer
 
 
-class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ReviewDetail(mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
-                   mixins.UpdateModelMixin, generics.GenericAPIView):
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+# class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin,
+#                  generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# class ReviewDetail(mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+#                    mixins.UpdateModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
--- watchlist/routes/stream.py
+++ watchlist/routes/stream.py
@@ -3,45 +3,55 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from watchlist.models import StreamPlatform
 from watchlist.serializers import StreamPlatformSerializer
+from rest_framework import generics
 
 
-class StreamPlatformListAV(APIView):
-    def get(self, request):
-        movies = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(movies,
-                                              many=True,
-                                              context={'request': request})
-        return Response(serializer.data)
+class StreamPlatformListAV(generics.ListCreateAPIView):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
 
-    def post(self, request):
-        serializer = StreamPlatformSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+class StreamPlatformDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StreamPlatform.objects.all
+    serializer_class = StreamPlatformSerializer
 
 
-class StreamPlatformDetailAV(APIView):
-    def get(self, request, id):
-        try:
-            movie = StreamPlatform.objects.get(id=id)
-            serializer = StreamPlatformSerializer(movie,
-                                                  context={'request': request})
-            return Response(serializer.data)
-        except StreamPlatform.DoesNotExist:
-            return Response({"error": "Streaming Plaform not found"},
-                            status=status.HTTP_404_NOT_FOUND)
+# class StreamPlatformListAV(APIView):
+#     def get(self, request):
+#         movies = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(movies,
+#                                               many=True,
+#                                               context={'request': request})
+#         return Response(serializer.data)
 
-    def put(self, request, id):
-        movie = StreamPlatform.objects.get(id=id)
-        serializer = StreamPlatformSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
 
-    def delete(self, request, id):
-        movie = StreamPlatform.objects.get(id=id)
-        movie.delete()
-        content = {'please move along': 'nothing to see here'}
-        return Response(content, status=status.HTTP_204_NO_CONTENT)
+
+# class StreamPlatformDetailAV(APIView):
+#     def get(self, request, id):
+#         try:
+#             movie = StreamPlatform.objects.get(id=id)
+#             serializer = StreamPlatformSerializer(movie,
+#                                                   context={'request': request})
+#             return Response(serializer.data)
+#         except StreamPlatform.DoesNotExist:
+#             return Response({"error": "Streaming Plaform not found"},
+#                             status=status.HTTP_404_NOT_FOUND)
+
+#     def put(self, request, id):
+#         movie = StreamPlatform.objects.get(id=id)
+#         serializer = StreamPlatformSerializer(movie, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, id):
+#         movie = StreamPlatform.objects.get(id=id)
+#         movie.delete()
+#         content = {'please move along': 'nothing to see here'}
+#         return Response(content, status=status.HTTP_204_NO_CONTENT)
