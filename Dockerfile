208365e0ec64c1a718b4e7c83baa669d53c4518f
Piyush Shukla <piyushshukla356@gmail.com>
2023-07-05 12:01:50 +0530

    feat: code clean

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,5 @@ COPY . .
 # Expose the port for the Django application
 EXPOSE 8000
 
+CMD ["python", "manage.py", "migrate"]
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- watchlist/routes/stream.py
+++ watchlist/routes/stream.py
@@ -1,36 +1,16 @@
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from watchlist.models import StreamPlatform as StreamPlatforms
-from watchlist.serializers import StreamPlatformSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from watchlist.models import StreamPlatform as StreamPlatforms
+from watchlist.serializers import StreamPlatformSerializer
 from django.shortcuts import get_object_or_404
 
 
 class StreamPlatform(viewsets.ModelViewSet):
     queryset = StreamPlatforms.objects.all()
     serializer_class = StreamPlatformSerializer
-
-
-# class StreamPlatform(viewsets.ViewSet):
-# def list(self, request):
-#     queryset = StreamPlatforms.objects.all()
-#     serializer = StreamPlatformSerializer(queryset, many=True)
-#     return Response(serializer.data)
-
-# def retrieve(self, request, pk=None):
-#     queryset = StreamPlatforms.objects.all()
-#     watchlist = get_object_or_404(queryset, pk=pk)
-#     serializer = StreamPlatformSerializer(watchlist)
-#     return Response(serializer.data)
-
-# def create(self, request):
-#     serializer = StreamPlatformSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(serializer.errors)
 
 
 class StreamPlatformListAV(generics.ListCreateAPIView):
@@ -43,42 +23,64 @@ class StreamPlatformDetailAV(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StreamPlatformSerializer
 
 
-# class StreamPlatformListAV(APIView):
-#     def get(self, request):
-#         movies = StreamPlatform.objects.all()
-#         serializer = StreamPlatformSerializer(movies,
-#                                               many=True,
-#                                               context={'request': request})
-#         return Response(serializer.data)
+"""
+Other method to create view
+class StreamPlatform(viewsets.ViewSet):
+def list(self, request):
+    queryset = StreamPlatforms.objects.all()
+    serializer = StreamPlatformSerializer(queryset, many=True)
+    return Response(serializer.data)
 
-#     def post(self, request):
-#         serializer = StreamPlatformSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors)
+def retrieve(self, request, pk=None):
+    queryset = StreamPlatforms.objects.all()
+    watchlist = get_object_or_404(queryset, pk=pk)
+    serializer = StreamPlatformSerializer(watchlist)
+    return Response(serializer.data)
 
-# class StreamPlatformDetailAV(APIView):
-#     def get(self, request, id):
-#         try:
-#             movie = StreamPlatform.objects.get(id=id)
-#             serializer = StreamPlatformSerializer(movie,
-#                                                   context={'request': request})
-#             return Response(serializer.data)
-#         except StreamPlatform.DoesNotExist:
-#             return Response({"error": "Streaming Plaform not found"},
-#                             status=status.HTTP_404_NOT_FOUND)
+def create(self, request):
+    serializer = StreamPlatformSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
 
-#     def put(self, request, id):
-#         movie = StreamPlatform.objects.get(id=id)
-#         serializer = StreamPlatformSerializer(movie, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class StreamPlatformListAV(APIView):
+    def get(self, request):
+        movies = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(movies,
+                                              many=True,
+                                              context={'request': request})
+        return Response(serializer.data)
 
-#     def delete(self, request, id):
-#         movie = StreamPlatform.objects.get(id=id)
-#         movie.delete()
-#         content = {'please move along': 'nothing to see here'}
-#         return Response(content, status=status.HTTP_204_NO_CONTENT)
+    def post(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class StreamPlatformDetailAV(APIView):
+    def get(self, request, id):
+        try:
+            movie = StreamPlatform.objects.get(id=id)
+            serializer = StreamPlatformSerializer(movie,
+                                                  context={'request': request})
+            return Response(serializer.data)
+        except StreamPlatform.DoesNotExist:
+            return Response({"error": "Streaming Plaform not found"},
+                            status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, id):
+        movie = StreamPlatform.objects.get(id=id)
+        serializer = StreamPlatformSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        movie = StreamPlatform.objects.get(id=id)
+        movie.delete()
+        content = {'please move along': 'nothing to see here'}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
+"""
